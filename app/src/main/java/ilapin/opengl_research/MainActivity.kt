d2636b8e7d5defe7312bf1cb17d4bf207eca3c0e
Igor Lapin <raynor73@gmail.com>
2020-01-24 00:42:39 +0300

    Init

---
+++ app/src/main/java/ilapin/opengl_research/MainActivity.kt
@@ -0,0 +1,41 @@
+package ilapin.opengl_research
+
+import android.content.res.Configuration
+import android.opengl.GLSurfaceView
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        if (resources.configuration.orientation == Configuration.ORIENTATION_LANDSCAPE) {
+            val renderer = GLSurfaceViewRenderer()
+            val glView = GLSurfaceView(this)
+            glView.setEGLContextClientVersion(2)
+            glView.setRenderer(renderer)
+            glView.renderMode = GLSurfaceView.RENDERMODE_CONTINUOUSLY
+            containerLayout.addView(glView, 0)
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        hideControls()
+    }
+
+    private fun hideControls() {
+        window.decorView.systemUiVisibility =
+            View.SYSTEM_UI_FLAG_LOW_PROFILE or
+                    View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY or
+                    View.SYSTEM_UI_FLAG_FULLSCREEN or
+                    View.SYSTEM_UI_FLAG_HIDE_NAVIGATION or
+                    View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION or
+                    View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or
+                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+    }
+}
