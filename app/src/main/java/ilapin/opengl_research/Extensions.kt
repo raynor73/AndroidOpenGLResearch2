c470a4f02781e59cc93fcbaf0220feecb908f8c4
Igor Lapin <raynor73@gmail.com>
2020-02-09 16:28:04 +0300

    Implement physics, patch #1

--- app/src/main/java/ilapin/opengl_research/Extensions.kt
+++ app/src/main/java/ilapin/opengl_research/Extensions.kt
@@ -2,8 +2,13 @@ package ilapin.opengl_research
 
 import android.opengl.GLES20
 import ilapin.opengl_research.domain.Mesh
+import org.joml.Quaternionf
 import org.joml.Vector2f
 import org.joml.Vector3f
+import org.ode4j.math.DQuaternion
+import org.ode4j.math.DQuaternionC
+import org.ode4j.math.DVector3
+import org.ode4j.math.DVector3C
 
 fun ilapin.engine3d.MeshComponent.toMesh(): Mesh {
     val convertedVertices = ArrayList<Mesh.Vertex>()
@@ -49,4 +54,29 @@ fun Boolean.toGLBoolean(): Int {
     } else {
         GLES20.GL_FALSE
     }
+}
+
+fun Vector3f.toVector(): DVector3 {
+    return DVector3(x.toDouble(), y.toDouble(), z.toDouble())
+}
+
+fun DVector3C.toVector(): Vector3f {
+    return Vector3f(get0().toFloat(), get1().toFloat(), get2().toFloat())
+}
+
+fun DVector3C.toVector(dest: Vector3f) {
+    dest.set(get0().toFloat(), get1().toFloat(), get2().toFloat())
+}
+
+fun Quaternionf.toQuaternion(): DQuaternion {
+    return DQuaternion(x.toDouble(), y.toDouble(), z.toDouble(), w.toDouble())
+}
+
+fun DQuaternionC.toQuaternion(): Quaternionf {
+    return Quaternionf(
+        get0().toFloat(),
+        get1().toFloat(),
+        get2().toFloat(),
+        get3().toFloat()
+    )
 }
---
+++ app/src/main/java/ilapin/opengl_research/domain/RigidBodyGameObjectComponent.kt
@@ -0,0 +1,52 @@
+package ilapin.opengl_research.domain
+
+import ilapin.common.android.log.L
+import ilapin.engine3d.GameObjectComponent
+import ilapin.engine3d.TransformationComponent
+import ilapin.opengl_research.App.Companion.LOG_TAG
+import ilapin.opengl_research.toVector
+import org.joml.Matrix4x3f
+import org.joml.Quaternionf
+import org.joml.Vector3f
+import org.ode4j.math.DVector3
+import org.ode4j.ode.DBody
+
+/**
+ * @author raynor on 08.02.20.
+ */
+class RigidBodyGameObjectComponent(val rigidBody: DBody) : GameObjectComponent() {
+
+    private val rotationMatrix = Matrix4x3f()
+    private val rotationQuaternion = Quaternionf()
+    private val column = DVector3()
+
+    private val tmpVector = Vector3f()
+
+    override fun update() {
+        super.update()
+
+        val transform = gameObject?.getComponent(TransformationComponent::class.java)
+            ?: error("No transform found for game object ${gameObject?.name}")
+
+        val rotationDMatrix3 = rigidBody.rotation
+
+        rotationDMatrix3.getColumn0(column)
+        column.toVector(tmpVector)
+        rotationMatrix.setColumn(0, tmpVector)
+
+        rotationDMatrix3.getColumn1(column)
+        column.toVector(tmpVector)
+        rotationMatrix.setColumn(1, tmpVector)
+
+        rotationDMatrix3.getColumn2(column)
+        column.toVector(tmpVector)
+        rotationMatrix.setColumn(2, tmpVector)
+
+        rotationQuaternion.setFromUnnormalized(rotationMatrix)
+
+        rigidBody.position.toVector(tmpVector)
+        L.d(LOG_TAG, "position: x = ${tmpVector.x}; y = ${tmpVector.y}; z = ${tmpVector.z}")
+        transform.position = tmpVector
+        transform.rotation = rotationQuaternion
+    }
+}
