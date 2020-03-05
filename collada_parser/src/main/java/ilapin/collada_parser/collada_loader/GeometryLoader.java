4b44666c42f901b7b9f8de95e41732504be85d50
Igor Lapin <igor.lapin73@gmail.com>
2020-03-06 01:06:00 +0300

    Implement alternate multiple animations support, patch 1

--- collada_parser/src/main/java/ilapin/collada_parser/collada_loader/GeometryLoader.java
+++ collada_parser/src/main/java/ilapin/collada_parser/collada_loader/GeometryLoader.java
@@ -21,12 +21,11 @@ import ilapin.collada_parser.xml_parser.XmlNode;
 public class GeometryLoader {
 
 	private static final Matrix4f CORRECTION = new Matrix4f().rotate((float) Math.toRadians(-90), new Vector3f(1, 0,0));
-	
-	//private final XmlNode meshData;
-	private final List<XmlNode> meshesData;
+
+	private final XmlNode meshData;
 
 	private final List<VertexSkinData> vertexWeights;
-	
+
 	private float[] verticesArray;
 	private float[] normalsArray;
 	private float[] texturesArray;
@@ -38,16 +37,12 @@ public class GeometryLoader {
 	List<Vector2f> textures = new ArrayList<Vector2f>();
 	List<Vector3f> normals = new ArrayList<Vector3f>();
 	List<Integer> indices = new ArrayList<Integer>();
-	
-	public GeometryLoader(final XmlNode geometryNode, final List<VertexSkinData> vertexWeights) {
+
+	public GeometryLoader(XmlNode geometryNode, List<VertexSkinData> vertexWeights) {
 		this.vertexWeights = vertexWeights;
-		meshesData = new ArrayList<>();
-		for (final XmlNode meshNode : geometryNode.getChildren("geometry")) {
-			meshesData.add(meshNode.getChild("mesh"));
-		}
-		//this.meshData = geometryNode.getChild("geometry").getChild("mesh");
+		this.meshData = geometryNode.getChild("geometry").getChild("mesh");
 	}
-	
+
 	public MeshData extractModelData(){
 		readRawData();
 		assembleVertices();
@@ -65,116 +60,64 @@ public class GeometryLoader {
 	}
 
 	private void readPositions() {
-		for (final XmlNode meshData : meshesData) {
-			final String positionsId = meshData.getChild("vertices").getChild("input").getAttribute("source").substring(1);
-			final XmlNode positionsData = meshData.getChildWithAttribute("source", "id", positionsId).getChild("float_array");
-			final int count = Integer.parseInt(positionsData.getAttribute("count"));
-			final String[] posData = positionsData.getData().split(" ");
-			for (int i = 0; i < count/3; i++) {
-				final float x = Float.parseFloat(posData[i * 3]);
-				final float y = Float.parseFloat(posData[i * 3 + 1]);
-				final float z = Float.parseFloat(posData[i * 3 + 2]);
-				final Vector4f position = new Vector4f(x, y, z, 1);
-				//Matrix4f.transform(CORRECTION, position, position);
-				vertices.add(new Vertex(vertices.size(), new Vector3f(position.x, position.y, position.z), vertexWeights.get(vertices.size())));
-			}
-		}
-		/*final String positionsId = meshData.getChild("vertices").getChild("input").getAttribute("source").substring(1);
-		final XmlNode positionsData = meshData.getChildWithAttribute("source", "id", positionsId).getChild("float_array");
-		final int count = Integer.parseInt(positionsData.getAttribute("count"));
-		final String[] posData = positionsData.getData().split(" ");
+		String positionsId = meshData.getChild("vertices").getChild("input").getAttribute("source").substring(1);
+		XmlNode positionsData = meshData.getChildWithAttribute("source", "id", positionsId).getChild("float_array");
+		int count = Integer.parseInt(positionsData.getAttribute("count"));
+		String[] posData = positionsData.getData().split(" ");
 		for (int i = 0; i < count/3; i++) {
-			final float x = Float.parseFloat(posData[i * 3]);
-			final float y = Float.parseFloat(posData[i * 3 + 1]);
-			final float z = Float.parseFloat(posData[i * 3 + 2]);
-			final Vector4f position = new Vector4f(x, y, z, 1);
+			float x = Float.parseFloat(posData[i * 3]);
+			float y = Float.parseFloat(posData[i * 3 + 1]);
+			float z = Float.parseFloat(posData[i * 3 + 2]);
+			Vector4f position = new Vector4f(x, y, z, 1);
 			//Matrix4f.transform(CORRECTION, position, position);
 			vertices.add(new Vertex(vertices.size(), new Vector3f(position.x, position.y, position.z), vertexWeights.get(vertices.size())));
-		}*/
+		}
 	}
 
 	private void readNormals() {
-		for (final XmlNode meshData : meshesData) {
-			final String normalsId = meshData.getChild("polylist").getChildWithAttribute("input", "semantic", "NORMAL")
-					.getAttribute("source").substring(1);
-			final XmlNode normalsData = meshData.getChildWithAttribute("source", "id", normalsId).getChild("float_array");
-			final int count = Integer.parseInt(normalsData.getAttribute("count"));
-			final String[] normData = normalsData.getData().split(" ");
-			for (int i = 0; i < count/3; i++) {
-				final float x = Float.parseFloat(normData[i * 3]);
-				final float y = Float.parseFloat(normData[i * 3 + 1]);
-				final float z = Float.parseFloat(normData[i * 3 + 2]);
-				final Vector4f norm = new Vector4f(x, y, z, 0f);
-				//Matrix4f.transform(CORRECTION, norm, norm);
-				normals.add(new Vector3f(norm.x, norm.y, norm.z));
-			}
-		}
-		/*final String normalsId = meshData.getChild("polylist").getChildWithAttribute("input", "semantic", "NORMAL")
+		String normalsId = meshData.getChild("polylist").getChildWithAttribute("input", "semantic", "NORMAL")
 				.getAttribute("source").substring(1);
-		final XmlNode normalsData = meshData.getChildWithAttribute("source", "id", normalsId).getChild("float_array");
-		final int count = Integer.parseInt(normalsData.getAttribute("count"));
-		final String[] normData = normalsData.getData().split(" ");
+		XmlNode normalsData = meshData.getChildWithAttribute("source", "id", normalsId).getChild("float_array");
+		int count = Integer.parseInt(normalsData.getAttribute("count"));
+		String[] normData = normalsData.getData().split(" ");
 		for (int i = 0; i < count/3; i++) {
-			final float x = Float.parseFloat(normData[i * 3]);
-			final float y = Float.parseFloat(normData[i * 3 + 1]);
-			final float z = Float.parseFloat(normData[i * 3 + 2]);
-			final Vector4f norm = new Vector4f(x, y, z, 0f);
+			float x = Float.parseFloat(normData[i * 3]);
+			float y = Float.parseFloat(normData[i * 3 + 1]);
+			float z = Float.parseFloat(normData[i * 3 + 2]);
+			Vector4f norm = new Vector4f(x, y, z, 0f);
 			//Matrix4f.transform(CORRECTION, norm, norm);
 			normals.add(new Vector3f(norm.x, norm.y, norm.z));
-		}*/
+		}
 	}
 
 	private void readTextureCoords() {
-		for (final XmlNode meshData : meshesData) {
-			final String texCoordsId = meshData.getChild("polylist").getChildWithAttribute("input", "semantic", "TEXCOORD")
-					.getAttribute("source").substring(1);
-			final XmlNode texCoordsData = meshData.getChildWithAttribute("source", "id", texCoordsId).getChild("float_array");
-			final int count = Integer.parseInt(texCoordsData.getAttribute("count"));
-			final String[] texData = texCoordsData.getData().split(" ");
-			for (int i = 0; i < count/2; i++) {
-				final float s = Float.parseFloat(texData[i * 2]);
-				final float t = Float.parseFloat(texData[i * 2 + 1]);
-				textures.add(new Vector2f(s, t));
-			}
-		}
-		/*final String texCoordsId = meshData.getChild("polylist").getChildWithAttribute("input", "semantic", "TEXCOORD")
+		String texCoordsId = meshData.getChild("polylist").getChildWithAttribute("input", "semantic", "TEXCOORD")
 				.getAttribute("source").substring(1);
-		final XmlNode texCoordsData = meshData.getChildWithAttribute("source", "id", texCoordsId).getChild("float_array");
-		final int count = Integer.parseInt(texCoordsData.getAttribute("count"));
-		final String[] texData = texCoordsData.getData().split(" ");
+		XmlNode texCoordsData = meshData.getChildWithAttribute("source", "id", texCoordsId).getChild("float_array");
+		int count = Integer.parseInt(texCoordsData.getAttribute("count"));
+		String[] texData = texCoordsData.getData().split(" ");
 		for (int i = 0; i < count/2; i++) {
-			final float s = Float.parseFloat(texData[i * 2]);
-			final float t = Float.parseFloat(texData[i * 2 + 1]);
+			float s = Float.parseFloat(texData[i * 2]);
+			float t = Float.parseFloat(texData[i * 2 + 1]);
 			textures.add(new Vector2f(s, t));
-		}*/
-	}
-	
-	private void assembleVertices(){
-		for (final XmlNode meshData : meshesData) {
-			final XmlNode poly = meshData.getChild("polylist");
-			final int typeCount = poly.getChildren("input").size();
-			final String[] indexData = poly.getChild("p").getData().split(" ");
-			for(int i=0;i<indexData.length/typeCount;i++){
-				final int positionIndex = Integer.parseInt(indexData[i * typeCount]);
-				final int normalIndex = Integer.parseInt(indexData[i * typeCount + 1]);
-				final int texCoordIndex = Integer.parseInt(indexData[i * typeCount + 2]);
-				processVertex(positionIndex, normalIndex, texCoordIndex);
-			}
 		}
-		/*final XmlNode poly = meshData.getChild("polylist");
-		final int typeCount = poly.getChildren("input").size();
-		final String[] indexData = poly.getChild("p").getData().split(" ");
-		for(int i=0;i<indexData.length/typeCount;i++){
-			final int positionIndex = Integer.parseInt(indexData[i * typeCount]);
-			final int normalIndex = Integer.parseInt(indexData[i * typeCount + 1]);
-			final int texCoordIndex = Integer.parseInt(indexData[i * typeCount + 2]);
-			processVertex(positionIndex, normalIndex, texCoordIndex);
-		}*/
 	}
-	
 
-	private Vertex processVertex(final int posIndex, final int normIndex, final int texIndex) {
-		final Vertex currentVertex = vertices.get(posIndex);
+	private void assembleVertices(){
+		XmlNode poly = meshData.getChild("polylist");
+		int typeCount = poly.getChildren("input").size();
+		String[] indexData = poly.getChild("p").getData().split(" ");
+		for(int i=0;i<indexData.length/typeCount;i++){
+			int positionIndex = Integer.parseInt(indexData[i * typeCount]);
+			int normalIndex = Integer.parseInt(indexData[i * typeCount + 1]);
+			int texCoordIndex = Integer.parseInt(indexData[i * typeCount + 2]);
+			processVertex(positionIndex, normalIndex, texCoordIndex);
+		}
+	}
+
+
+	private Vertex processVertex(int posIndex, int normIndex, int texIndex) {
+		Vertex currentVertex = vertices.get(posIndex);
 		if (!currentVertex.isSet()) {
 			currentVertex.setTextureIndex(texIndex);
 			currentVertex.setNormalIndex(normIndex);
@@ -196,13 +139,13 @@ public class GeometryLoader {
 	private float convertDataToArrays() {
 		float furthestPoint = 0;
 		for (int i = 0; i < vertices.size(); i++) {
-			final Vertex currentVertex = vertices.get(i);
+			Vertex currentVertex = vertices.get(i);
 			if (currentVertex.getLength() > furthestPoint) {
 				furthestPoint = currentVertex.getLength();
 			}
-			final Vector3f position = currentVertex.getPosition();
-			final Vector2f textureCoord = textures.get(currentVertex.getTextureIndex());
-			final Vector3f normalVector = normals.get(currentVertex.getNormalIndex());
+			Vector3f position = currentVertex.getPosition();
+			Vector2f textureCoord = textures.get(currentVertex.getTextureIndex());
+			Vector3f normalVector = normals.get(currentVertex.getNormalIndex());
 			verticesArray[i * 3] = position.x;
 			verticesArray[i * 3 + 1] = position.y;
 			verticesArray[i * 3 + 2] = position.z;
@@ -211,7 +154,7 @@ public class GeometryLoader {
 			normalsArray[i * 3] = normalVector.x;
 			normalsArray[i * 3 + 1] = normalVector.y;
 			normalsArray[i * 3 + 2] = normalVector.z;
-			final VertexSkinData weights = currentVertex.getWeightsData();
+			VertexSkinData weights = currentVertex.getWeightsData();
 			jointIdsArray[i * 3] = weights.jointIds.get(0);
 			jointIdsArray[i * 3 + 1] = weights.jointIds.get(1);
 			jointIdsArray[i * 3 + 2] = weights.jointIds.get(2);
@@ -223,16 +166,16 @@ public class GeometryLoader {
 		return furthestPoint;
 	}
 
-	private Vertex dealWithAlreadyProcessedVertex(final Vertex previousVertex, final int newTextureIndex, final int newNormalIndex) {
+	private Vertex dealWithAlreadyProcessedVertex(Vertex previousVertex, int newTextureIndex, int newNormalIndex) {
 		if (previousVertex.hasSameTextureAndNormal(newTextureIndex, newNormalIndex)) {
 			indices.add(previousVertex.getIndex());
 			return previousVertex;
 		} else {
-			final Vertex anotherVertex = previousVertex.getDuplicateVertex();
+			Vertex anotherVertex = previousVertex.getDuplicateVertex();
 			if (anotherVertex != null) {
 				return dealWithAlreadyProcessedVertex(anotherVertex, newTextureIndex, newNormalIndex);
 			} else {
-				final Vertex duplicateVertex = new Vertex(vertices.size(), previousVertex.getPosition(), previousVertex.getWeightsData());
+				Vertex duplicateVertex = new Vertex(vertices.size(), previousVertex.getPosition(), previousVertex.getWeightsData());
 				duplicateVertex.setTextureIndex(newTextureIndex);
 				duplicateVertex.setNormalIndex(newNormalIndex);
 				previousVertex.setDuplicateVertex(duplicateVertex);
@@ -243,7 +186,7 @@ public class GeometryLoader {
 
 		}
 	}
-	
+
 	private void initArrays(){
 		this.verticesArray = new float[vertices.size() * 3];
 		this.texturesArray = new float[vertices.size() * 2];
@@ -253,7 +196,7 @@ public class GeometryLoader {
 	}
 
 	private void removeUnusedVertices() {
-		for (final Vertex vertex : vertices) {
+		for (Vertex vertex : vertices) {
 			vertex.averageTangents();
 			if (!vertex.isSet()) {
 				vertex.setTextureIndex(0);
@@ -261,5 +204,5 @@ public class GeometryLoader {
 			}
 		}
 	}
-	
+
 }
