a2b188018f57847ba95cd4966ac374c2b808d659
Igor Lapin <raynor73@gmail.com>
2020-03-06 18:07:03 +0300

    Implement alternate multiple animations support, patch 3

--- app/src/main/assets/scenes/character_animation_scene/scene.js
+++ app/src/main/assets/scenes/character_animation_scene/scene.js
@@ -9,9 +9,66 @@
 // var quaternionsPool = ...
 // var vectorsPool = ...
 
+var pixelDensityFactor;
+var displayWidth;
+var displayHeight;
+
+var uiCamera;
+
+var fpsText;
+var fpsTextTransform;
+var fpsCalculator;
+
 function start() {
+    pixelDensityFactor = displayMetricsRepository.getPixelDensityFactor();
+    displayWidth = displayMetricsRepository.displayWidth;
+    displayHeight = displayMetricsRepository.displayHeight;
+
+    uiCamera = findGameObject(scene.rootGameObject, "ui_camera");
+
+    var fpsTextGameObject = findGameObject(scene.rootGameObject, "fps_text");
+    fpsTextTransform = scene.getTransformationComponent(fpsTextGameObject);
+    fpsText = scene.getTextComponent(fpsTextGameObject);
+    fpsCalculator = new FpsCalculator();
+
+    layoutUi()
+
     scene.getSkeletalAnimatorComponent(findGameObject(scene.rootGameObject, "player")).start();
 }
 
 function update(dt) {
+    updateFps(dt);
+}
+
+function layoutUi() {
+    var orthoCamera = scene.getOrthoCameraComponent(uiCamera);
+    orthoCamera.left = 0;
+    orthoCamera.right = displayWidth;
+    orthoCamera.bottom = 0;
+    orthoCamera.top = displayHeight;
+
+    layoutFpsText();
+}
+
+function layoutFpsText() {
+    var scale = vectorsPool.obtain();
+    var position = vectorsPool.obtain();
+
+    scale.set(fpsTextTransform.scale);
+    scale.mul(pixelDensityFactor);
+    scale.z = 1;
+    fpsTextTransform.scale = scale;
+
+    position.set(fpsTextTransform.position);
+    position.x = scale.x / 2;
+    position.y = displayHeight - scale.y / 2;
+    fpsTextTransform.position = position;
+
+    vectorsPool.recycle(position);
+    vectorsPool.recycle(scale);
+}
+
+function updateFps(dt) {
+    fpsCalculator.update(dt);
+    fpsText.text = ("FPS: " + fpsCalculator.fps).substring(0, 10);
 }
